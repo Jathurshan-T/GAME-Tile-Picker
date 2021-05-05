e85b7c51e95693f70444c7e0c6b4e4364ebc26d8
jathu-git <jathu.theiv@gmail.com>
2021-05-05 02:25:51 -0400

    testing

--- TilePicker/Program.cs
+++ TilePicker/Program.cs
@@ -8,9 +8,7 @@ namespace TilePicker
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        // entry point
         [STAThread]
         static void Main()
         {
